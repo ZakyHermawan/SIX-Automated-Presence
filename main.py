7bfd4f51e3365b9d779cd6cda9051c1c62dcec06
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2020-09-07 12:07:24 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import json
+import time
+import re
+
+try:
+  driver = webdriver.Firefox()
+
+  # Main page
+  driver.get('https://akademik.itb.ac.id/')
+  driver.find_element_by_id('login').click()
+
+  # Login page
+  driver.find_element_by_xpath('//a[@href=\'/login/INA\']').click()
+
+  # INA login page
+  with open('credentials.json', 'r') as f:
+    credentials = json.load(f)
+
+  driver.find_element_by_id("username").send_keys(credentials['username'])
+  driver.find_element_by_id("password").send_keys(credentials['password'])
+  driver.find_element_by_xpath('//input[@type=\'submit\']').click()
+
+  # SIX Dashboard
+  # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//a[contains(@href, \'/app/mahasiswa:{}/kelas\')]'.format(credentials["nim"])))).click()
+  class_link = driver.find_element_by_xpath('//a[contains(@href, \'/app/mahasiswa:{}/kelas\')]'.format(credentials["nim"]))
+  driver.execute_script("arguments[0].click();", class_link)
+
+  # SIX Class Menu
+  current_day_cell = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME, 'bg-info')))
+  classes_today = current_day_cell.find_elements_by_xpath('.//div/div[@title]/a[@class=\'linkpertemuan\']')
+  classes_schedule = list(map(lambda val: re.sub(r'(..:..-..:..).*', r'\1', val.text).split('-'), classes_today))
+
+  t = time.localtime()
+  current_time = time.strftime("%H:%M", t)
+  print(current_time)
+
+  # for index, schedule in enumerate(classes_schedule):
+  #   start_time, end_time = schedule[0], schedule[1]
+  #   if(start_time < current_time < end_time):
+  #     classes_today[index].click()
+  #     popup_dialog = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//div[@role=\'dialog\']')))
+  #     popup_dialog.find_element_by_xpath('.//button[@name=\'form[hadir]\']').click()
+  #     print('Presence successful.')
+
+  # else:
+  #   raise Exception('You\'re currently not attending any class.')
+  
+  # For testing purposes
+  classes_today[0].click()
+  popup_dialog = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//div[@role=\'dialog\']')))
+  popup_dialog.find_element_by_xpath('.//button[@name=\'form[hadir]\']').click()
+
+  # print(classes_schedule)
+  # driver.find_element_by_xpath()
+except Exception as exc:
+  print(exc)
+finally:
+  time.sleep(10)
+  driver.close()

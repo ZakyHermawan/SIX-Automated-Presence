9928af064ede7328dbae315ba61e0b003a08bda6
Matthew Kevin Amadeus <mkamadeus.mka@gmail.com>
2020-09-07 14:41:01 +0700

    Fixed presence button not pressed bug

--- main.py
+++ main.py
@@ -12,13 +12,16 @@ try:
   driver = webdriver.Firefox()
 
   # Main page
+  print('Opening main site...')
   driver.get('https://akademik.itb.ac.id/')
   driver.find_element_by_id('login').click()
 
   # Login page
+  print('Opening akademik.itb.ac.id login page...')
   driver.find_element_by_xpath('//a[@href=\'/login/INA\']').click()
 
   # INA login page
+  print('Login using ITB SSO...')
   with open('credentials.json', 'r') as f:
     credentials = json.load(f)
 
@@ -27,28 +30,31 @@ try:
   driver.find_element_by_xpath('//input[@type=\'submit\']').click()
 
   # SIX Dashboard
-  # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//a[contains(@href, \'/app/mahasiswa:{}/kelas\')]'.format(credentials["nim"])))).click()
+  print('Opening SIX dashboard....')
   class_link = driver.find_element_by_xpath('//a[contains(@href, \'/app/mahasiswa:{}/kelas\')]'.format(credentials["nim"]))
   driver.execute_script("arguments[0].click();", class_link)
 
   # SIX Class Menu
+  print('Opening SIX class menu....')
   current_day_cell = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME, 'bg-info')))
   classes_today = current_day_cell.find_elements_by_xpath('.//div/div[@title]/a[@class=\'linkpertemuan\']')
   classes_schedule = list(map(lambda val: re.sub(r'(..:..-..:..).*', r'\1', val.text).split('-'), classes_today))
 
   # Get current time
+  print('Opening SIX class menu....')
   t = time.localtime()
   current_time = time.strftime("%H:%M", t)
   print(current_time)
 
+  print('Finding current class....')
   for index, schedule in enumerate(classes_schedule):
     start_time, end_time = schedule[0], schedule[1]
     if(start_time <= current_time <= end_time):
-      classes_today[index].click()
+      driver.execute_script("arguments[0].click();", classes_today[index])
       popup_dialog = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//div[@role=\'dialog\']')))
-      popup_dialog.find_element_by_xpath('.//button[@name=\'form[hadir]\']').click()
+      popup_dialog.find_element_by_id('form_hadir').click()
       print('Presence successful.')
-
+      break
   else:
     raise Exception('You\'re currently not attending any class.')
   

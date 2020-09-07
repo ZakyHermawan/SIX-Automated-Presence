10ee477e6ee2a14b892ac590aa7e25d5cfe1b49b
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2020-09-07 20:49:52 +0700

    :fire: Updated logging code and readme

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+import logging
 import json
 import time
 import re
 
+logging.basicConfig(format='[%(asctime)s] - %(message)s', datefmt='%d %b %H:%M:%S', level=logging.INFO)
+
 try:
-  driver = webdriver.Firefox()
+  
+  logging.info('Opening webdriver...')
+  options = Options()
+  options.add_argument("--headless")
+  driver = webdriver.Firefox(options=options)
+
+  time.sleep(8)
 
   # Main page
-  print('Opening main site...')
+  logging.info('Opening main site...')
   driver.get('https://akademik.itb.ac.id/')
   driver.find_element_by_id('login').click()
 
   # Login page
-  print('Opening akademik.itb.ac.id login page...')
+  logging.info('Opening akademik.itb.ac.id login page...')
   driver.find_element_by_xpath('//a[@href=\'/login/INA\']').click()
 
   # INA login page
-  print('Login using ITB SSO...')
+  logging.info('Login using ITB SSO...')
   with open('credentials.json', 'r') as f:
     credentials = json.load(f)
 
@@ -30,32 +40,34 @@ try:
   driver.find_element_by_xpath('//input[@type=\'submit\']').click()
 
   # SIX Dashboard
-  print('Opening SIX dashboard....')
+  logging.info('Opening SIX dashboard....')
   class_link = driver.find_element_by_xpath('//a[contains(@href, \'/app/mahasiswa:{}/kelas\')]'.format(credentials["nim"]))
   driver.execute_script("arguments[0].click();", class_link)
 
   # SIX Class Menu
-  print('Opening SIX class menu....')
-  current_day_cell = WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME, 'bg-info')))
+  logging.info('Opening SIX class menu....')
+  current_day_cell = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'bg-info')))
   classes_today = current_day_cell.find_elements_by_xpath('.//div/div[@title]/a[@class=\'linkpertemuan\']')
   classes_schedule = list(map(lambda val: re.sub(r'(..:..-..:..).*', r'\1', val.text).split('-'), classes_today))
 
   # Get current time
-  print('Opening SIX class menu....')
   t = time.localtime()
   current_time = time.strftime("%H:%M", t)
-  print(current_time)
 
-  print('Finding current class....')
+  # Input presence current attended class
+  logging.info('Finding attended class....')
+  found = False
   for index, schedule in enumerate(classes_schedule):
     start_time, end_time = schedule[0], schedule[1]
     if(start_time <= current_time <= end_time):
       driver.execute_script("arguments[0].click();", classes_today[index])
-      popup_dialog = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//div[@role=\'dialog\']')))
-      popup_dialog.find_element_by_id('form_hadir').click()
-      print('Presence successful.')
+      presence_button = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'form_hadir')))
+      presence_button.click()
+      logging.info('Presence successful.')
+      found=True
       break
-  else:
+  
+  if(not found):
     raise Exception('You\'re currently not attending any class.')
   
   # For testing purposes
@@ -64,15 +76,19 @@ try:
   # presence_button = popup_dialog.find_element_by_xpath('.//button[@name=\'form[hadir]\']')
   # presence_button.click()
 
-  # print(classes_schedule)
+  # logging.info(classes_schedule)
   # driver.find_element_by_xpath()
 except NoSuchElementException as exc:
-  print('Presence failed. Some things are possible:')
-  print('> Your class does not use the SIX presence system')
-  print('> Your class have not opened the presence form yet')
-  print('> You have already filled the presence form')
+  logging.exception(
+  '''
+    Presence failed. Some things are possible:
+    > Your class have not opened the presence form yet
+    > You have already filled the presence form
+    > Your class does not use the SIX presence system
+  '''
+  )
 except Exception as exc:
-  print(exc)
+  logging.exception(exc)
 finally:
-  time.sleep(10)
+  time.sleep(5)
   driver.close()
--- readme.md
+++ readme.md
@@ -4,11 +4,14 @@
 
 # Pendahuluan
 
-Buat absen di SIX ITB. CRON not included, klo mo fork silahkan saja. Masih testing sekarang.
+![Working proof](./proof.gif)
+
+Buat absen di SIX ITB. CRON not included, silakan fork untuk kepentingan bersama.
 
 # Prerequisites
 
-Punya Python. Kalo gapunya ya gabisa.
+- Punya Python. Kalo gapunya ya gabisa.
+- Untuk Selenium harus disetup sendiri; cari driver browser yang sesuai. Dalam code ini saya menggunakan [geckodriver](https://github.com/mozilla/geckodriver/releases) untuk Firefox. Ikutin petunjuk yang ada, masukin ke _*PATH*_.
 
 # Setup
 
@@ -36,12 +39,8 @@ pip install -r requirements.txt
 
 3. Bikin file `credentials.json`, ada contohnya.
 
-4. Kalo dah masuk, tinggal jalanin scriptnya.
+4. Kalo dah masuk, tinggal jalanin scriptnya. Di repo bakal ditaroh di mode headless biar ga muncul browsernya kaya di gif.
 
 ```bash
 python main.py
 ```
-
-# Note
-
-Script ini tidak termasuk CRON/scheduling. Kalo ada yang mau improve silahkan fork buat kepentingan bersama.

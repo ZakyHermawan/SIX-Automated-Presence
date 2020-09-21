7e64efb41c04085c2363adba8ad29fae125a409d
Jonathan Yudi Gunawan <jonathan.gunawan@xtremax.com>
2020-09-21 22:50:23 +0700

    added code enum, direct login to INA, remove --disable-gpu

--- presence.py
+++ presence.py
@@ -4,23 +4,32 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from enum import Enum
 import time
 import re
 
 import logging
 logging.basicConfig(format='[%(asctime)s] - %(message)s', datefmt='%d %b %H:%M:%S', level=logging.INFO)
 
+class Code(Enum):
+    DRIVER_INIT_ERROR = 0
+    INVALID_LOGIN = 1
+    NO_CLASS = 2
+    PRESENCE_NOT_OPENED_YET = 3
+    SUCCESS = 4
+    PRESENCE_ALREADY_FILLED = 5
+
+
 message = {
-    0: "Failed to init driver",
-    1: "Invalid password",
-    2: "There's currently no class",
-    3: "Presence form not opened yet",
-    4: "Presence form filled successfully",
-    5: "Presence form already filled",
-    6: "Failed to run",
+    Code.DRIVER_INIT_ERROR: "Failed to init driver",
+    Code.INVALID_LOGIN: "Invalid password",
+    Code.NO_CLASS: "There's currently no class",
+    Code.PRESENCE_NOT_OPENED_YET: "Presence form not opened yet",
+    Code.SUCCESS: "Presence form filled successfully",
+    Code.PRESENCE_ALREADY_FILLED: "Presence form already filled",
 }
 def presence(username, password, success_callback=lambda: None, fail_callback=lambda: None, delay=8, headless=True, options=None):
-    code = 0
+    code = Code.DRIVER_INIT_ERROR
     classcode = ""
 
     # Presence
@@ -31,7 +40,6 @@ def presence(username, password, success_callback=lambda: None, fail_callback=la
             options = Options()
             if(headless):
                 options.add_argument("--headless")
-                options.add_argument('--disable-gpu')
         args = {
             "options": options,
         }
@@ -49,14 +57,15 @@ def presence(username, password, success_callback=lambda: None, fail_callback=la
                 cnt += 1
                 error = e
         else:
-            logging.error("Fail to init webdriver (" + str(cnt) + " times) | " + str(error))
+            logging.error("Fail to init webdriver after retrying " + str(cnt) + " times | " + str(error))
             raise error
 
+        # Presencing...
         code, classcode = presence_util(driver, username, password, delay)
-        if code != 4:
-            fail_callback(code, classcode, message[code])
-        else:
+        if code == Code.SUCCESS:
             success_callback(classcode, message[code])
+        else:
+            fail_callback(code, classcode, message[code])
 
     except Exception as exc:
         logging.exception(exc)
@@ -68,17 +77,9 @@ def presence(username, password, success_callback=lambda: None, fail_callback=la
 def presence_util(driver, username, password, delay):
     driver.implicitly_wait(0.1)
 
-    # Main page
-    logging.info('Opening main site')
-    driver.get('https://akademik.itb.ac.id/')
-    driver.find_element_by_id('login').click()
-
-    # Login page
-    logging.info('Opening akademik.itb.ac.id login page')
-    driver.find_element_by_xpath('//a[@href=\'/login/INA\']').click()
-
-    # INA login page
-    logging.info('Login using ITB SSO')
+    # INA Login page
+    logging.info('Opening INA login site')
+    driver.get('https://login.itb.ac.id/cas/login?service=https%3A%2F%2Fakademik.itb.ac.id%2Flogin%2FINA')
     driver.find_element_by_id("username").send_keys(username)
     driver.find_element_by_id("password").send_keys(password)
     driver.find_element_by_xpath('//input[@type=\'submit\']').click()
@@ -89,7 +90,7 @@ def presence_util(driver, username, password, delay):
     try:
         _ = re.search(".*mahasiswa:(.*?)/.*?", fullhtml).group(1)  # nim
     except Exception:
-        return (1, "")
+        return (Code.INVALID_LOGIN, "")
     class_link = driver.find_elements_by_xpath("//div[contains(@class, 'apps')]//div[contains(@class, 'col-xs-4 col-sm-3 col-md-2 text-center')]")[3]
     class_link.click()
 
@@ -98,7 +99,7 @@ def presence_util(driver, username, password, delay):
     current_day_cell = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'bg-info')))
     classes_today = current_day_cell.find_elements_by_xpath('.//div/div[@title]/a')
     classes_schedule = list(map(lambda val: re.sub(r'(..:..-..:..).*', r'\1', val.text).split('-'), classes_today))
-    classes_code = list(map(lambda val: val.text[12:18], classes_today))
+    classes_code = list(map(lambda val: val.text[12:], classes_today))
 
     # Get current server time (GMT +7)
     t = time.gmtime(time.time() + 25200)
@@ -121,7 +122,7 @@ def presence_util(driver, username, password, delay):
             try:
                 _ = driver.find_element_by_id('form_tidakhadir')
                 logging.info('Presence form is filled for {}'.format(classes_code[index]))
-                return (5, classes_code[index])
+                return (Code.PRESENCE_ALREADY_FILLED, classes_code[index])
             except Exception:
                 pass
 
@@ -130,11 +131,11 @@ def presence_util(driver, username, password, delay):
                 presence_button = driver.find_element_by_id('form_hadir')
                 presence_button.click()
                 logging.info('Presence successful')
-                return (4, classes_code[index])
+                return (Code.SUCCESS, classes_code[index])
             except Exception:
                 logging.info('Presence form currently closed for {}'.format(classes_code[index]))
-                return(3, classes_code[index])
+                return(Code.PRESENCE_NOT_OPENED_YET, classes_code[index])
 
         else:
             logging.info('Currently is not the time for {}'.format(classes_code[index]))
-    return (2, "")
+    return (Code.NO_CLASS, "")
